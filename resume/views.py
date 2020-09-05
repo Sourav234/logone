1136f4f67bfb8f9c67a3aeee3c189ea1d39bf629
Sourav234 <sourabhdhanuka1234@gmail.com>
2020-09-06 04:04:02 +0530

    ome

--- resume/views.py
+++ resume/views.py
@@ -6,9 +6,6 @@ from django.views import generic
 # Create your views here.
 
 
-def home(request):
-    return render(request,'home.html')
-
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
