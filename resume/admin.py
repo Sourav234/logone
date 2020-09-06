bdaa2e162e60c88f9e48063f65880bf0b5a93171
Sourav234 <sourabhdhanuka1234@gmail.com>
2020-09-06 14:24:13 +0530

    lapto

--- resume/admin.py
+++ resume/admin.py
@@ -1,25 +1,2 @@
 from django.contrib import admin
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from django.contrib.auth import user
 
-class MyUserCreationForm(UserCreationForm):
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-        try:
-            user._default_manager.get(username=username)
-        except user.DoesNotExist:
-            return username
-        raise forms.ValidationError(self.error_messages['duplicate_username'])
-
-    class Meta(UserCreationForm.Meta):
-        model = user
-
-
-class UserAdmin(AuthUserAdmin):
-    add_form = MyUserCreationForm
-    update_form_class = UserChangeForm
-
-
-admin.site.register(user, UserAdmin)
